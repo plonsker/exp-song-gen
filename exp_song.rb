13391ace27f41c265d8a72747bb7dd77d5efaf82
joe <joeplonsker@gmail.com>
2016-09-07 22:47:09 -0400

    clean up method

--- exp_song.rb
+++ exp_song.rb
@@ -1,11 +1,8 @@
-def exp_song_gen(num)
-  notes = ["C", "C#" "D", "D#","E", "F", "F#", "G", "G#", "A", "A#", "B"].sample
-  frets = (0..11).to_a.sample(num)
-  
+def exp_song_gen
 
+  num = (2..8).to_a.sample
+  notes = ["C", "C#" "D", "D#","E", "F", "F#", "G", "G#", "A", "A#", "B"].sample(num)
+
+  puts notes
 
 end
-
-num_of_notes = (2..8).to_a.sample
-
-exp_song_gen(num_of_notes)
