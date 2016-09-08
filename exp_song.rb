49caa304b95016d45f9195e19d1ab2c9ad20a8c8
joe <joeplonsker@gmail.com>
2016-09-07 23:01:24 -0400

    Fix typo in array that combined C# and D into one string

--- exp_song.rb
+++ exp_song.rb
@@ -1,7 +1,7 @@
 def exp_song_gen
 
   num = (2..8).to_a.sample
-  notes = ["C", "C#" "D", "D#","E", "F", "F#", "G", "G#", "A", "A#", "B"].sample(num)
+  notes = ["C", "C#", "D", "D#","E", "F", "F#", "G", "G#", "A", "A#", "B"].sample(num)
 
   puts notes
 
